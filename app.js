719c5e14585f6a2db5f5b0e9185d2394088f3518
Stephan Garrett <stephan.garrett@yahoo.com>
2020-06-24 16:46:58 +0000

    added pages for math and linked them, added js for addition

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-javascript will go here!
+//javascript will go here!
+
+//Addition Scripts
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const input = document.getElementById('answer');
+const ans = document.getElementById('answer').value;
+const response = document.getElementById('response');
+const easy = 5;
+const numOne = Math.round(Math.random()*easy);
+const numTwo = Math.round(Math.random()*easy);
+const answer = numOne + numTwo;
+
+
+num1.innerHTML = numOne;
+num2.innerHTML = numTwo;
+
+
+input.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    document.getElementById("go").click();
+  }
+});
+
+function getInputValue(){
+  const ans = document.getElementById('answer').value;
+  if (ans == answer) {
+    response.innerHTML = "Yay!";
+  } else {
+    response.innerHTML = "Oh no!";
+  }
+};
