5b474ab8bf4fb14fb50171ee561965c7c32f08bd
Stephan Garrett <stephan.garrett@yahoo.com>
2020-06-26 14:54:12 +0000

    Added buttons for math functionality, experimented with options to control buttons

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 //javascript will go here!
 
+
+//Button Managment
+//const difficultyButton = document.getElementsByClassName('difficultyButton');
+//
+//difficultyButton.addEventListener("click", (e) => {
+//  e.target.remove('difficultyButton');
+//  e.target.add('difficultyButtonSelected');
+//});
+
+function buttonChange() {
+  const difficultyButton = document.getElementById('singleDigitMath');
+  difficultyButton.classList.remove('difficultyButton');
+  difficultyButton.classList.add('difficultyButtonSelected');
+}
+
+//function changeClass(id)
+//{
+//    if ($('#' + id).hasClass('difficultyButton'))
+//        $('#' + id).removeClass('difficultyButton'),
+//        $('#' + id).addClass('difficultyButtonSelected');
+//    else
+//    {
+//        console.log('error');      
+//    }
+//}
+
+
 //Addition Scripts
 const num1 = document.getElementById('num1');
 const num2 = document.getElementById('num2');
 const input = document.getElementById('answer');
 const ans = document.getElementById('answer').value;
 const response = document.getElementById('response');
-const easy = 5;
-const numOne = Math.round(Math.random()*easy);
-const numTwo = Math.round(Math.random()*easy);
+var difficulty = null;
+difficulty = 5;
+const numOne = Math.round(Math.random()*difficulty);
+const numTwo = Math.round(Math.random()*difficulty);
 const answer = numOne + numTwo;
 
 
