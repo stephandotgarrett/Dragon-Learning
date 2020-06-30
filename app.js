ac6f77728b4c8bdf96e49605ee3321b6e525b151
Stephan Garrett <stephangarrett@MacBook-Pro.local>
2020-06-29 20:00:01 -0400

    Addition page styling added, worked on difficulty selcetion button functionality, still needs a lot of work

--- app.js
+++ app.js
@@ -1,49 +1,41 @@
 //javascript will go here!
-
-
-//Button Managment
-//const difficultyButton = document.getElementsByClassName('difficultyButton');
-//
-//difficultyButton.addEventListener("click", (e) => {
-//  e.target.remove('difficultyButton');
-//  e.target.add('difficultyButtonSelected');
-//});
-
-function buttonChange() {
-  const difficultyButton = document.getElementById('singleDigitMath');
-  difficultyButton.classList.remove('difficultyButton');
-  difficultyButton.classList.add('difficultyButtonSelected');
-}
-
-//function changeClass(id)
-//{
-//    if ($('#' + id).hasClass('difficultyButton'))
-//        $('#' + id).removeClass('difficultyButton'),
-//        $('#' + id).addClass('difficultyButtonSelected');
-//    else
-//    {
-//        console.log('error');      
-//    }
-//}
-
-
-//Addition Scripts
-const num1 = document.getElementById('num1');
+onst num1 = document.getElementById('num1');
 const num2 = document.getElementById('num2');
 const input = document.getElementById('answer');
 const ans = document.getElementById('answer').value;
 const response = document.getElementById('response');
 var difficulty = null;
-difficulty = 5;
-const numOne = Math.round(Math.random()*difficulty);
-const numTwo = Math.round(Math.random()*difficulty);
+let numOne = Math.round(Math.random()*difficulty);
+let numTwo = Math.round(Math.random()*difficulty);
+// let startSelection = document.getElementById('singleDigitMath');
+// startSelection.classList += " active";
+
+//Button Managment
+
+// Add active class to the current button (highlight it)
+var difficultySelectionDiv = document.getElementById("difficultySelection");
+var btns = difficultySelectionDiv.getElementsByClassName("difficultyButton");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+
+  });
+}
+
+
+
+//Addition Scripts
+c
 const answer = numOne + numTwo;
 
 
 num1.innerHTML = numOne;
 num2.innerHTML = numTwo;
 
-
+//Allows user to press enter to input math answer
 input.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
     e.preventDefault();
